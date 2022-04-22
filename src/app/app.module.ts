84db9a52328c0b3600fb782314ea829f70d37be6
HenrryJK <henrryorbezo@upeu.edu.pe>
2022-04-22 10:08:58 -0500

    #1 Intentando hacer conexion con firebase y que levante

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
+// Importamos clases de firebase
+    import { AngularFireModule } from '@angular/fire/compat';
+    import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+    import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+    /// importamos clase de la conexion del firebase
+    import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
